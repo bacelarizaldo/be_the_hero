efd3f7e0b4efddaeb7686b590fe5c7dd71a285ca
bacelarizaldo <izaldo.figueiroa.b@gmail.com>
2020-03-26 12:27:09 -0300

    Back-end da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          VOCÊ ACABA DE RECEBER CORONA VIRUS.
+        </p>
+        <a
+          className="App-link"
+          href="https://youtu.be/u-rtfOx1aQM?t=48"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CLICK AQUI PARA SE CURAR
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
